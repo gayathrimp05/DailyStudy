2c46e8d81b270c118c51a92578c8acb70ea915f9
Gayathri-mp <gayathrimp5648@gmail.com>
2026-04-08 23:52:53 +0530

    included day04 file inside src folder

--- frontend/Dailystudy/src/App.jsx
+++ frontend/Dailystudy/src/App.jsx
@@ -1,16 +1,7 @@
-import { useState } from "react";
+import Day04 from "./day04";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      <h1>{count}</h1>
-
-      <button onClick={() => setCount(count + 1)}>+</button>
-      <button onClick={() => setCount(count - 1)}>-</button>
-    </div>
-  );
+  return <Day04 />;
 }
 
 export default App;
