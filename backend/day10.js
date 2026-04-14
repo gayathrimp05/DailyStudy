4546475d37b307e1be25d47a3c2b9fc47eaeecfd
Gayathri-mp <gayathrimp5648@gmail.com>
2026-04-14 23:27:27 +0530

    outfit based on the weather-api

---
+++ backend/day10.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/outfit", (req, res) => {
+    const weather = req.body.weather;
+    const temp = req.body.temperature;
+
+    let outfit = "";
+
+    if (weather === "rainy") {
+        outfit = "Raincoat + Waterproof shoes";
+    } 
+    else if (weather === "cold" || temp < 20) {
+        outfit = "Jacket + Hoodie";
+    } 
+    else if (weather === "hot" || temp > 30) {
+        outfit = "T-shirt + Shorts";
+    } 
+    else {
+        outfit = "Casual wear";
+    }
+
+    res.json({ outfit });
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
